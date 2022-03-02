b40ee02ad3e1a579f474ab83f60f145834d59712
Phydon <leann.phydon@gmail.com>
2022-03-02 21:05:06 +0100

    Changes and removed extern crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use std::path::PathBuf;
 use std::{env, fs};
-// extern crate walkdir;
-// use walkdir::Walkdir;
 
 fn main() {
     let mut args = Vec::new();
@@ -18,26 +17,28 @@ fn main() {
     }
 
     let current_path = env::current_dir().unwrap();
+    if !file_in_dir(&current_path, &args) {
+        eprintln!("Your file doesn`t exist in the current directory");
+    }
+}
 
-    // let recursive_dir = Walkdir::new(current_path);
-    // for entry in recursive_dir {
-    //     println!("{}", entry.unwrap().path().display());
-    // }
-
+fn file_in_dir(dir: &PathBuf, parameters: &Vec<String>) -> bool {
+    let mut counter: u32 = 0;
     // list all files in current directory
-    for entry in fs::read_dir(&current_path).unwrap() {
+    for entry in fs::read_dir(&dir).unwrap() {
         let entry = entry.unwrap().path();
         // println!("entry = {}", entry.display());
 
         // if argument in current directory, print path
         let path_str = entry.to_str().unwrap();
-        if path_str.contains(&args[0]) {
-            println!(
-                "You can find the file {:?} in here: [ {:?} ]",
-                args[0], path_str
-            );
+        if path_str.contains(&parameters[0]) {
+            counter += 1;
+            println!("=> [ {:?} ]", path_str);
         }
     }
-    // TODO 
-    eprintln!("Your file doesn`t exists in the current directory");
+    if counter != 0 {
+        return true;
+    } else {
+        return false;
+    }
 }
